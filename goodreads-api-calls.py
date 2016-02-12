78d5b75a9d30500cf6a436b9e7c915a5a5c8aa4f
Emma Ferguson <emmamferguson@gmail.com>
2016-02-11 18:01:57 -0800

    refactored goodreads-api code into functions; now adds books to DB instead of to lists or dictionaries

--- goodreads-api-calls.py
+++ goodreads-api-calls.py
@@ -2,51 +2,135 @@ import os
 import requests
 import json
 import xmltodict
-from goodreads import client
+import pdb
+from model import Book, User, Recommendation, UserBook
 
 goodreads_key=os.environ['GOODREADS_KEY']
 goodreads_secret=os.environ['GOODREADS_SECRET']
 
-# gc = client.GoodreadsClient(goodreads_key, goodreads_secret)
-# get user id
-
+# get user id (this will be via OAuth probably)
 user_id = "16767050"
 
-# get user info
-user = requests.get('https://www.goodreads.com/user/show.xml?key=$%s&v=2&id=%s' % (goodreads_key, user_id))
-# this returns an XML; now we parse:
-user_info = xmltodict.parse(user.content)
 
-shelves = {}
-# extract user shelves: name, id, book count; eventually this should save to DB
-for user_shelf in user_info['GoodreadsResponse']['user']['user_shelves']['user_shelf']:
-    shelves[user_shelf['name']] = {'id': user_shelf['id']['#text'], 'item_count':user_shelf['book_count']['#text']}
+def get_shelves(user_id):
+    """Pulls user's shelves out of their user info."""
+    user = requests.get('https://www.goodreads.com/user/show.xml?key=$%s&v=2&id=%s' % (goodreads_key, user_id))
+    user_info = xmltodict.parse(user.content)
+    # initialize user shelf dictionary
+    shelves = {}
+    # extract user shelves: name, id, book count; eventually this should save to DB
+    for user_shelf in user_info['GoodreadsResponse']['user']['user_shelves']['user_shelf']:
+        shelf_name = user_shelf['name']
+        shelf_id = user_shelf['id']['#text']
+        num_of_books = int(user_shelf['book_count']['#text'])
+        num_pages = (num_of_books/200) + 1
+        shelves[shelf_name] = {'id': shelf_id, 'item_count': num_of_books, 'pages': num_pages}
 
-# initialize user shelf dictionary
-all_user_books = {}
+    return shelves
 
-# For each shelf: get book info name, author, goodreads ID, ISBN13, publish year, user rating
-for shelf in shelves.keys():
-    # figure out how many pages of 200 books per page are needed for each shelf
-    if int(shelves[shelf]['item_count']) < 201:
-        per_page = int(shelves[shelf]['item_count'])
-        pages = 1
-    else:
-        per_page = 200
-        pages = (int(shelves[shelf]['item_count'])/200) + 1
 
-    books = []
-    for page_num in range(1,pages+1):
-        shelf_response = requests.get('https://www.goodreads.com/review/list.xml?key=$%s&v=2&id=%s&shelf=%s&per_page=%d&page=%d' % (goodreads_key, user_id, shelf, per_page, page_num))
-        parsed_shelf = xmltodict.parse(shelf_response.content)
-        for book in parsed_shelf['GoodreadsResponse']['reviews']['review']:
-            books.append(book['book']['title'])
-        # print shelf, page_num, len(books)
-    all_user_books[shelf] = books
-    # print shelf, len(books)
+def fetch_subject_info(book_id):
+    """Based on a book's NextBook BID, fetch subject info. 
+    Calls OpenLib & GoogleBooks APIs."""
+    pass
 
-for key in all_user_books.keys():
-    print key, len(all_user_books[key])
 
+def get_books_from_shelves(shelves):
+    """Takes in dictionary of user's shelves; returns list of all books on shelves."""
+
+    all_books = []
+
+    for shelf in shelves.keys():
+        pages = shelves[shelf]['pages']
+        for page in range(1,pages+1):
+            shelf_response = requests.get('https://www.goodreads.com/review/list.xml?key=$%s&v=2&id=%s&shelf=%s&per_page=200&page=%d' 
+                                            % (goodreads_key, user_id, shelf, page))
+            parsed_shelf = xmltodict.parse(shelf_response.content)
+            for book in parsed_shelf['GoodreadsResponse']['reviews']['review']:
+                all_books.append((shelf, book))
+    return all_books
+
+
+
+
+def add_book_to_library(book_list):
+    """Adds book to the library table if it does not already exist."""
+    for item in book_list:
+        book = item[1]
+        book_info = book['book']
+        isbn = book_info['isbn13']
+        if Book.query.filter_by(isbn=isbn) is False:
+            newbook = Book(title = book_info['title'], 
+                        goodreads_bid = book['id'],
+                        author = book_info['authors']['author']['name'],
+                        isbn = book_info['isbn13'],
+                        pub_year = book_info['publication_year'],
+                        pages = book_info['num_pages'],
+                        publisher = book_info['publisher'],
+                        img_url = book_info['image_url'],
+                        goodreads_url = book_info['link'],
+                        get_subjects = 1)
+            db.session.add(newbook)
+
+
+def add_userbook_to_userbooks(book_list, user_id):
+    """Add a new userbook to the userbooks table."""
+     
+    for item in book_list:
+        book = item[1] 
+        shelf = item[0]
+        book_info = book['book']
+        isbn = book_info['isbn13']
+        new_volume = Book.query.filter_by(isbn = isbn)
+
+        if shelf in ['read', 'currently-reading']:
+            status = 'read'
+        elif shelf == 'to-read':
+            status = 'want-to-read'
+        else:
+            status = None
+
+        new_userbook = UserBook(user_id = user_id,
+                                book_id = book_id,
+                                gr_shelf_name = shelf, 
+                                gr_shelf_id = shelves[shelf]['id'],
+                                source = "goodreads",
+                                status = status,
+                                rating = book['rating'] )
+        db.session.add(new_userbook)
+        db.session.commit()
+
+
+
+
+###################################
+# FUNCTION CALLS
+
+shelves = get_shelves(user_id)
+
+book_list = get_books_from_shelves(shelves)
+
+# check if book in library; if not, add to library
+add_book_to_library(book_list)
+
+# add book to UserBook table
+add_userbook_to_userbooks(book_list, user_id)
+
+
+######################################################################
+# GET SUBJECT BOOK INFO
+# for each book in library:
+    # if has get_subjects = True:
+    # go out and get book data from OpenLib
+    # extract subjects
+        # for each subject
+            # if not in subjects, add to subjects
+            # add to subject-book with book id
+
+    # go get book info from GoogleBooks
+    # extract categories
+        # for each category
+            # if not in subjects, add to subjects
+            # add to subject-book with book id
 
 
--- model.py
+++ model.py
@@ -17,9 +17,9 @@ class User(db.Model):
     goodreads_uid = db.Column(db.Integer, nullable=False)
     password = db.Column(db.String(15), nullable=False)
     rec_frequency = db.Column(db.Integer, nullable=False, default=1)
-    sign_up_date = db.Column(db.Datetime, nullable=False)
+    sign_up_date = db.Column(db.DateTime, nullable=False)
     paused = db.Column(db.Integer, nullable=False, default=0)
-    paused_date = db.Column(db.Datetime)
+    paused_date = db.Column(db.DateTime, nullable=True)
 
 
 
@@ -39,7 +39,10 @@ class Book(db.Model):
     preview = db.Column(db.String(200))
     pages = db.Column(db.Integer)
     publisher = db.Column(db.String(100))
-
+    language = db.Column(db.String(15))
+    img_url = db.Column(db.String(100))
+    goodreads_url = db.Column(db.String(100))
+    get_subjects = db.Column(db.Integer, default = 0)
 
 
 class Recommendation(db.Model):
@@ -49,10 +52,10 @@ class Recommendation(db.Model):
 
     rec_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     userbook_id = db.Column(db.Integer, db.ForeignKey('user_books.userbook_id'))
-    date_created = db.Column(db.Datetime, nullable=False)
-    date_provided = db.Column(db.Datetime)
+    date_created = db.Column(db.DateTime, nullable=False)
+    date_provided = db.Column(db.DateTime, nullable=True)
     # response: read_now, read_later, already_read, reject
-    response = db.Column(db.String(20))
+    response = db.Column(db.String(20), nullable=True)
  
 
 
@@ -63,12 +66,12 @@ class UserBook(db.Model):
     userbook_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     book_id = db.Column(db.Integer, db.ForeignKey('library.book_id'))
-    gr_shelf_id = db.Column(db.Integer)
-    gr_shelf_name = db.Column(db.String(50))
+    gr_shelf_id = db.Column(db.Integer, nullable=True)
+    gr_shelf_name = db.Column(db.String(50), nullable=True)
     #status: read, currently_reading, want_to_read, rec_no_response, not_to_read
-    status = db.Column(db.String(50)) 
-    source = db.Column(db.String(50))
-    rating = db.Column(db.Integer)
+    status = db.Column(db.String(50), nullable=False) 
+    source = db.Column(db.String(50), nullable=True)
+    rating = db.Column(db.Integer, nullable=True)
 
 
     book = db.relationship("Book",
@@ -85,13 +88,18 @@ class Subject(db.Model):
 
     __tablename__ = "subjects"
 
-    subject_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    subject = db.Column(db.String(100), nullable=False)
+    subject_id = db.Column(db.Integer, 
+                        autoincrement=True, 
+                        primary_key=True)
+    subject = db.Column(db.String(100), 
+                        nullable=False)
     # source = google_books, open_lib, possibly others?
-    source = db.Column(db.String(25))
+    source = db.Column(db.String(25), nullable=False)
 
 
-    books = db.relationship("Book", secondary = 'book_subjects', backref = "subjects")
+    books = db.relationship("Book", 
+            secondary = 'book_subjects', 
+            backref = "subjects")
 
 
 class BookSubject(db.Model):
@@ -100,7 +108,9 @@ class BookSubject(db.Model):
 
     __tablename__ = "book_subjects"
 
-    booksubject_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    booksubject_id = db.Column(db.Integer, 
+                            autoincrement=True, 
+                            primary_key=True)
     book_id = db.Column(db.Integer, db.ForeignKey('library.book_id'))
     subject_id = db.Column(db.Integer, db.ForeignKey('subjects.subject_id'))
 
@@ -139,5 +149,3 @@ if __name__ == "__main__":
 
     connect_to_db(app)
     print "Connected to DB."
-
-
