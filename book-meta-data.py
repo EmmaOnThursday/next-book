eab128f241efa9910952b90e837c0bf88be3df47
Emma Ferguson <emmamferguson@gmail.com>
2016-02-13 00:34:26 -0800

    beginning to write book metadata api calls

--- book-meta-data.py
+++ book-meta-data.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import xmltodict
+import json
 import pdb
 from model import Book, User, Recommendation, UserBook, connect_to_db, db
 from server import app
@@ -14,28 +15,49 @@ def fetch_book_metadata():
     Calls OpenLib & GoogleBooks APIs.
     Savs data to library table."""
 
-    need_language = Book.query.filter(Book.language).all()   
+    # get original pub date, language from book.show method in GR API
 
-    for book in need_subjects:
+    need_language = Book.query.filter(Book.language.is_(None)).all()   
+    # language_count = 0
+
+    for book in need_language:
+        response = requests.get("https://www.goodreads.com/book/show/%s.json?key=%s" % (book.goodreads_bid, goodreads_key))
+        parsed_response = json.loads(response.content)
+        book_info = parsed_response['GoodreadsResponse']
+        # pdb.set_trace()
+        book.language = book_info['book']['language_code']
+        # book.original_pub_year = int(book_info['book']['work']['original_publication_year']['#text'])
+        # language_count += 1
+    # return language_count
+
+        # db.session.add(book)
+    # db.session.flush()
+###################################
+# FUNCTION CALLS
+
+connect_to_db(app)
+
+books_updated = fetch_book_metadata()
 
 
 
 
-def fetch_subject_info():
-    """Based on a book's ISBN or Goodreads ID, fetch subject info. 
-    Calls OpenLib & GoogleBooks APIs."""
-
-    need_subjects = Book.query.filter(Book.get_subjects ==1).all()   
-
-    for book in need_subjects:
 
 
-    # for each book: 
-        # get ISBN
-        # OpenLib API call
-        # save subjects to subjects table
-        # get subject_ID
-        # save subject_ID & book_ID to book-subjects table
-        # wait 30 seconds
-        # get original pub date, language from book.show method in GR API
-    pass
+# def fetch_subject_info():
+#     """Based on a book's ISBN or Goodreads ID, fetch subject info. 
+#     Calls OpenLib & GoogleBooks APIs."""
+
+#     need_subjects = Book.query.filter(Book.get_subjects ==1).all()   
+
+#     for book in need_subjects:
+
+
+#     # for each book: 
+#         # get ISBN
+#         # OpenLib API call
+#         # save subjects to subjects table
+#         # get subject_ID
+#         # save subject_ID & book_ID to book-subjects table
+#         # wait 30 seconds
+#     pass
--- goodreads-api-calls.py
+++ goodreads-api-calls.py
@@ -31,7 +31,8 @@ def get_shelves(gr_user_id):
 
 
 def get_books_from_shelves(shelves):
-    """Takes in dictionary of user's shelves; returns list of all books on shelves."""
+    """Takes in dictionary of user's shelves; returns list of all books on shelves.
+    Return list: books stored in tuples: (shelf name, book info)."""
 
     all_books = []
 
@@ -47,21 +48,16 @@ def get_books_from_shelves(shelves):
 
 
 
+##### ADD BOOK ####
+
 def add_book_to_library(book_list):
     """Adds book to the library table if it does not already exist."""
     for item in book_list:
         book = item[1]
         book_info = book['book']
         current_isbn = book_info['isbn13']
-        current_gr_bid = book['id']
-
-        # does this goodreads ID exist in my db?
-            # yes: stop right there
-            # no: does this book have an ISBN?
-                # yes: does that ISBN exist in my DB?
-                    # yes: stop right there
-                    # no: create a book
-                # no: save book with ISBN of 0
+        current_gr_bid = book_info['id']['#text']
+        # pdb.set_trace()
 
         # tries to instantiate a book
         this_book = db.session.query(Book).filter_by(goodreads_bid = current_gr_bid).first()
@@ -72,14 +68,11 @@ def add_book_to_library(book_list):
                 this_book = db.session.query(Book).filter_by(isbn = current_isbn).first()
             else:
                 current_isbn = "0"
-        
-        # if this_book:
-            # print "book exists at id ", this_book.book_id
 
         # if that fails, create a new book
         if not this_book:
             newbook = Book(title = book_info['title'], 
-                            goodreads_bid = book['id'],
+                            goodreads_bid = current_gr_bid,
                             author = book_info['authors']['author']['name'],
                             isbn = current_isbn,
                             pub_year = book_info['publication_year'],
@@ -91,9 +84,10 @@ def add_book_to_library(book_list):
             db.session.add(newbook)
 
     db.session.commit()
-            # print "new book created at id ", newbook.book_id
 
 
+
+###### ADD USERBOOK ######
 def add_userbook_to_userbooks(book_list, gr_user_id):
     """Add a new userbook to the userbooks table."""
     
@@ -101,12 +95,12 @@ def add_userbook_to_userbooks(book_list, gr_user_id):
     print len(book_list)
 
     for item in book_list:
-        # pdb.set_trace()
         book = item[1] 
         shelf = item[0]
         book_info = book['book']
-        gr_bid = int(book['id'])
-        print gr_bid, "type= ", type(gr_bid)
+        gr_bid = int(book_info['id']['#text'])
+        print "###############", gr_bid, type(gr_bid)
+        # pdb.set_trace()
         current_isbn = book_info['isbn13']
 
         #set status
@@ -118,34 +112,30 @@ def add_userbook_to_userbooks(book_list, gr_user_id):
             status = None
 
         # try to get book from library with goodreads ID
-        library_record = Book.query.filter_by(goodreads_bid = gr_bid).first()
+        library_record = Book.query.filter_by(goodreads_bid=gr_bid).first()
         print type(library_record)
 
         # if book does not exist w/ goodreads id, try to get it with ISBN
         if not library_record:
             print "reached line 138"
             if len(current_isbn) == 13:
-                library_record = db.session.query(Book).filter_by(isbn = current_isbn).first()
+                library_record = db.session.query(Book).filter_by(isbn=current_isbn).first()
                 print "reached line 139"
         if not library_record:
             print "Failed to record user rating for ", book_list[1][1]['book']['title'], "user = ", current_user.email
-        
-        new_volume = UserBook.query.filter(UserBook.user_id==current_user.user_id, 
-                                            UserBook.book_id == library_record.book_id).first()
-        
-        if new_volume: 
-            print "have this record already at userbook_id", new_volume.userbook_id
+        else:
+            new_volume = UserBook.query.filter(UserBook.user_id==current_user.user_id, UserBook.book_id == library_record.book_id).first()
 
-        if not new_volume:
-            new_userbook = UserBook(user_id = current_user.user_id,
-                                    book_id = library_record.book_id,
-                                    gr_shelf_name = shelf, 
-                                    gr_shelf_id = shelves[shelf]['id'],
-                                    source = "goodreads",
-                                    status = status,
-                                    rating = book['rating'])
-            db.session.add(new_userbook)
-            print "created new record at userbook_id", new_userbook.userbook_id
+            if not new_volume:
+                new_userbook = UserBook(user_id = current_user.user_id,
+                                        book_id = library_record.book_id,
+                                        gr_shelf_name = shelf, 
+                                        gr_shelf_id = shelves[shelf]['id'],
+                                        source = "goodreads",
+                                        status = status,
+                                        rating = book['rating'])
+                db.session.add(new_userbook)
+                print "created new record at userbook_id", new_userbook.userbook_id
     db.session.commit()
 
 
@@ -164,22 +154,5 @@ add_book_to_library(book_list)
 
 # add book to UserBook table
 add_userbook_to_userbooks(book_list, gr_user_id)
-
-
-######################################################################
-# GET SUBJECT BOOK INFO
-# for each book in library:
-    # if has get_subjects = True:
-    # go out and get book data from OpenLib
-    # extract subjects
-        # for each subject
-            # if not in subjects, add to subjects
-            # add to subject-book with book id
-
-    # go get book info from GoogleBooks
-    # extract categories
-        # for each category
-            # if not in subjects, add to subjects
-            # add to subject-book with book id
 
 
